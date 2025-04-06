cfeabdd3b4702e80ef0b95d6972e56dccb686304
ilyes rejeb <ilyes.rejeb@isimg.tn>
2025-04-06 19:13:47 +0200

    Update sign.php

--- sign.php
+++ sign.php
@@ -65,13 +65,13 @@ if ($role == 'Pharmacien') {
                 $mail->isSMTP();
                 $mail->Host = 'smtp.gmail.com';  // Utilisation du serveur SMTP de Gmail
                 $mail->SMTPAuth = true;          // Activation de l'authentification SMTP
-                $mail->Username = 'pharfind@gmail.com';  // Remplacez par votre adresse e-mail Gmail
-                $mail->Password = 'rfqdlvatmnuklgtb';  // Utilisez le mot de passe d'application
+                $mail->Username = '';  // Remplacez par votre adresse e-mail Gmail
+                $mail->Password = '';  // Utilisez le mot de passe d'application
                 $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;  // Utilisation de TLS pour sécuriser la connexion
                 $mail->Port = 587;  // Port utilisé par Gmail pour TLS
 
                 // Informations de l'expéditeur et du destinataire
-                $mail->setFrom('pharfind@gmail.com', 'PharmFind');
+                $mail->setFrom('', 'PharmFind');
                 $mail->addAddress($email, $nom);  // Remplacez par l'adresse du pharmacien
 
                 // Contenu de l'e-mail
@@ -163,13 +163,13 @@ if ($role == 'Pharmacien') {
                     $mail->isSMTP();
                     $mail->Host = 'smtp.gmail.com';  // Utilisation du serveur SMTP de Gmail
                     $mail->SMTPAuth = true;          // Activation de l'authentification SMTP
-                    $mail->Username = 'pharfind@gmail.com';  // Remplacez par votre adresse e-mail Gmail
-                    $mail->Password = 'rfqdlvatmnuklgtb';  // Utilisez le mot de passe d'application
+                    $mail->Username = '';  // Remplacez par votre adresse e-mail Gmail
+                    $mail->Password = '';  // Utilisez le mot de passe d'application
                     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;  // Utilisation de TLS pour sécuriser la connexion
                     $mail->Port = 587;  // Port utilisé par Gmail pour TLS
     
                     // Informations de l'expéditeur et du destinataire
-                    $mail->setFrom('pharfind@gmail.com', 'PharmFind');
+                    $mail->setFrom('', 'PharmFind');
                     $mail->addAddress($email, $nom);  // Remplacez par l'adresse du pharmacien
     
                     // Contenu de l'e-mail
